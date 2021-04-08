9b2463afe2b40a817b9538224b5a906778c3adf2
iqballerr <ilodin@uci.edu>
2021-04-08 03:26:12 -0700

    Added BeforeEach

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,13 +1,21 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
+	
+	GettingStarted g;
+	
+	@BeforeEach
+	public void setup() {
+		g = new GettingStarted();
+	}
 
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = g.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
@@ -15,13 +23,13 @@ public class GettingStartedTest {
 
     @Test
     public void addFiveToZero() {
-        int result = new GettingStarted().addFive(0);
+        int result = g.addFive(0);
         Assertions.assertEquals(5, result);
     }
 
     @Test
     public void addFiveToMinus20() {
-        int result = new GettingStarted().addFive(-20);
+        int result = g.addFive(-20);
         Assertions.assertEquals(-15,result);
     }
 }
